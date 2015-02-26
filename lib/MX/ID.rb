8f534dd29055061bcd0a62bffd2276daf6a93afc
Pablo Ruiz <pjruiz@maquech.com.mx>
2015-02-26 03:45:49 -0600

    bloque aseugrado apra I18n

--- lib/MX/ID.rb
+++ lib/MX/ID.rb
@@ -7,5 +7,7 @@ require "MX/ID/version"
 require "MX/ID/auxiliar"
 require "MX/ID/RFC"
 
-I18n.enforce_available_locales = false
-I18n.default_locale = :es
+if defined?(I18n) # Dependencia de numbers_and_words
+  I18n.enforce_available_locales = false
+  I18n.default_locale = :es
+end
